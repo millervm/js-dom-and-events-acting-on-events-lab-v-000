9d7c3375c8cf6b49a095c9c5a28021c306cd0a6b
Veronica Miller <vmcmiller@gmail.com>
2019-05-13 04:42:14 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+};
+
+function addNewElementAsLi() {
+  document.querySelector('ul.employee-list').append(retrieveEmployeeInformation());
+};
+
+function addNewLiOnClick() {
+  document.querySelector('input').addEventListener("click", addNewElementAsLi());
+  document.querySelector('input').value = "";
+};
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector("a[href='#']").addEventListener("click", function() {
+    document.querySelector('ul.employee-list').innerHTML = "";
+  });
+};
