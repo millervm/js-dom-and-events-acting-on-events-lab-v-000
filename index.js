6e8997e90d68e1ac0f71c19666068b4bbb911007
Veronica Miller <vmcmiller@gmail.com>
2019-05-13 03:24:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
-  return $('input[name="name"]').attr("value");
+  $('input[name="name"]').attr("value");
 };
 
 function addNewElementAsLi() {
